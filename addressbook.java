a509ede63d11ff887ef328078a04d7359f33366c
baluguru <smkumarag@gmail.com>
2020-11-07 10:41:35 +0530

    added code to write_file_and_check_its_existence

--- addressbook.java
+++ addressbook.java
@@ -1,24 +1,42 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class WriteFile 
+{
 
-public class ReadFile {
-  public static void main(String[] args) {
-    try {
-	  Scanner scanner = new Scanner(System.in);
-	  System.out.print("Enter file name: ");
-	  String fileName = scanner.next();
-	  File myObj = new File("C:\\Users\\baluguru\\Desktop\\AddressBook\\"+fileName);
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-  }
+	public static void main(String[] args) 
+	{		
+		Scanner scanner = new Scanner(System.in);
+     	System.out.print("Enter file name: ");
+	   String fileName = scanner.next();
+		File log = new File("C:\\Users\\baluguru\\Desktop\\AddressBook\\"+fileName);
+
+				try
+				{
+				    if(!log.exists())
+				    {
+				        System.out.println("We had to make a new file.");
+				        log.createNewFile();
+				    }
+
+				   FileWriter fileWriter = new FileWriter(log, true);
+
+				   BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+					System.out.print("Enter Text to write: ");
+					String text = scanner.next();
+					//Object timeStamp ="newtext";
+					bufferedWriter.write("******* " + text.toString() +"******* " + "\n");
+				   bufferedWriter.close();
+				   System.out.println("Done");
+				} 
+				catch(IOException e) 
+				{
+				    System.out.println("COULD NOT WRITE!!");
+				}
+		
+		}
+
 }
 
