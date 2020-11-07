3ca418a9784f89f53c212bfb01e8b3d561615e0d
baluguru <smkumarag@gmail.com>
2020-11-07 10:36:41 +0530

    added read_file_and_check_its_existence

--- addressbook.java
+++ addressbook.java
@@ -1,31 +1,24 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-public class CreateFile 
-{
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
-	public static void main(String[] args) 
-	{
-		try 
-		{   Scanner scanner = new Scanner(System.in);
-		    System.out.print("Enter file name: ");
-     		String fileName = scanner.next();
-    		File myObj = new File("C:\\Users\\baluguru\\Desktop\\AddressBook\\"+fileName);
-			if (myObj.createNewFile())
-			{
-				System.out.println("File created: "+ myObj.getName());
-
-			}  else 
-			{
-				System.out.println("File already exists.");
-			}
-		} 
-		catch (IOException e) 
-		{
-			System.out.println("An error occured.");
-			e.printStackTrace();
-		}
-		
-
-		}
+public class ReadFile {
+  public static void main(String[] args) {
+    try {
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.print("Enter file name: ");
+	  String fileName = scanner.next();
+	  File myObj = new File("C:\\Users\\baluguru\\Desktop\\AddressBook\\"+fileName);
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 }
+
