936c44a029cd04da0a8f83352ac05087d1431adb
baluguru <smkumarag@gmail.com>
2020-11-07 10:32:25 +0530

    added create_file_and_check_its_existence

--- addressbook.java
+++ addressbook.java
@@ -1,1 +1,31 @@
-welcome to address book problem!
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+public class CreateFile 
+{
+
+	public static void main(String[] args) 
+	{
+		try 
+		{   Scanner scanner = new Scanner(System.in);
+		    System.out.print("Enter file name: ");
+     		String fileName = scanner.next();
+    		File myObj = new File("C:\\Users\\baluguru\\Desktop\\AddressBook\\"+fileName);
+			if (myObj.createNewFile())
+			{
+				System.out.println("File created: "+ myObj.getName());
+
+			}  else 
+			{
+				System.out.println("File already exists.");
+			}
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("An error occured.");
+			e.printStackTrace();
+		}
+		
+
+		}
+}
